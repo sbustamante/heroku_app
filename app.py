6c8ff0b570750f3fe53ec67e24b71641167d53ce
Sebastian Bustamante <macsebas33@gmail.com>
2022-03-15 23:41:59 +0100

    Simple dash version

--- app.py
+++ app.py
@@ -1,60 +1,32 @@
-import dash
-from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
-
-import flask
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
-import time
-import os
 
-app = dash.Dash(__name__)
+app = Dash(__name__)
 server = app.server
-server.secret_key = os.environ.get('secret_key', 'secret')
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/hello-world-stock.csv')
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
-app = dash.Dash('app', server=server)
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.scripts.config.serve_locally = False
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
-app.layout = html.Div([
-    html.H1('Stock Tickers'),
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Tesla', 'value': 'TSLA'},
-            {'label': 'Apple', 'value': 'AAPL'},
-            {'label': 'Coke', 'value': 'COKE'}
-        ],
-        value='TSLA'
-    ),
-    dcc.Graph(id='my-graph')
-], className="container")
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
 
-@app.callback(Output('my-graph', 'figure'),
-              [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown_value):
-    dff = df[df['Stock'] == selected_dropdown_value]
-    return {
-        'data': [{
-            'x': dff.Date,
-            'y': dff.Close,
-            'line': {
-                'width': 3,
-                'shape': 'spline'
-            }
-        }],
-        'layout': {
-            'margin': {
-                'l': 30,
-                'r': 20,
-                'b': 30,
-                't': 20
-            }
-        }
-    }
-
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
